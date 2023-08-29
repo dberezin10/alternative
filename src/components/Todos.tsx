de628211cf4878652a93a97ad988afcc8ace3071
denisberezin <dberezin@ippontech.ru>
2023-08-29 13:01:40 +0300

    #11: mutation

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,11 +1,15 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import TodosServices from "../services/TodosServices";
+import { useState } from "react";
 
 export interface ITodoResponse {
   id: number;
+  userId: number;
   title: string;
   completed: boolean;
 }
+
+export type ICreateTodo = Omit<ITodoResponse, "id">;
 
 const Todos = (): JSX.Element => {
   const {
@@ -13,21 +17,54 @@ const Todos = (): JSX.Element => {
     error,
     data: todoData,
   } = useQuery({
-    queryKey: ["todos"],
+    queryKey: ["t"],
     queryFn: TodosServices.getAllIdTodos,
     select: ({ data }) => data,
   });
+
+  const [value, setValue] = useState("");
+
+  const { mutate } = useMutation({
+    mutationKey: ["create todo"],
+    mutationFn: TodosServices.create(),
+  });
+
+  // console.log("createTodo", createTodo);
 
   if (isLoading) return <h1>Loading...</h1>;
 
   if (error) return <h1>An error has occurred:</h1>;
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("value", value);
+    mutate(value);
+  };
+
   return (
-    <div>
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "1fr 1fr",
+        gap: 10,
+        border: "1px solid black",
+      }}
+    >
       <h1>TODOS LIST</h1>
       {todoData?.map((el) => (
         <p key={el.id}>{el.title}</p>
       ))}
+
+      <h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+          <button>Clicked</button>
+        </form>
+      </h2>
     </div>
   );
 };
--- src/services/TodosServices.ts
+++ src/services/TodosServices.ts
@@ -1,18 +1,29 @@
 import httpClient from "../utils/httpClient";
-import { ITodoResponse } from "../components/Todos";
+import { ICreateTodo, ITodoResponse } from "../components/Todos";
 import { AxiosResponse } from "axios";
 
 interface ITodosServices {
   getAllIdTodos: () => Promise<AxiosResponse<ITodoResponse[]>>;
   getById: (id: number) => Promise<ITodoResponse>;
+  create: () => Promise<string[]>;
 }
 
 class TodosServices implements ITodosServices {
   async getAllIdTodos(): Promise<AxiosResponse<ITodoResponse[]>> {
-    return httpClient.get("/todos");
+    return httpClient.get("/todos?_start=0&_limit=5");
   }
   async getById(id: number): Promise<ITodoResponse> {
     return httpClient.get(`/todos/${id}`).then((res) => res.data);
+  }
+
+  async create(): Promise<string[]> {
+    return httpClient
+      .post("todos", {
+        title: "11",
+        completed: false,
+        userId: 1,
+      })
+      .then((data) => data.data);
   }
 }
 
