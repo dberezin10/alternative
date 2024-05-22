a6cc35b0cdf911cc9cfb8caa1fca76f5567871be
denisberezin <dberezin@ippontech.ru>
2024-05-22 14:42:04 +0300

    #48: add binarySearch

--- algoritms.js
+++ algoritms.js
@@ -308,3 +308,30 @@ const breadthSearch2 = (graph, start, end) => {
 };
 
 console.log("breadthSearch2", breadthSearch2(graph, "a", "g"));
+
+const binarySearch = (sortedArray, key) => {
+  let start = 0;
+  let end = sortedArray.length - 1;
+
+  while (start <= end) {
+    let middle = Math.floor((start + end) / 2);
+
+    if (sortedArray[middle] === key) {
+      // found the key
+      return middle;
+    } else if (sortedArray[middle] < key) {
+      // continue searching to the right
+      start = middle + 1;
+    } else {
+      // search searching to the left
+      end = middle - 1;
+    }
+  }
+  // key wasn't found
+  return -1;
+};
+
+// Пример использования
+const sortedArray = [1, 3, 5, 7, 9, 11, 13, 15, 17];
+const targetElement = 9;
+const index = binarySearch(sortedArray, targetElement);
